1528c5abda2aeff46636538b045745beba2383ee
Victor Winberg <victor.m.winberg@gmail.com>
2020-07-17 11:05:18 +0200

    Lodash global (#5)
    * Lodash global
    
    * Update Navbar.vue

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,5 @@
 import Vue, { VNode } from "vue";
+import _ from "lodash/fp";
 import "@mdi/font/css/materialdesignicons.css";
 
 import App from "./App.vue";
@@ -9,6 +10,8 @@ import "./plugins/bueify";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Object.defineProperty(Vue.prototype, "$_", { value: _ });
 
 export const bus = new Vue();
 
